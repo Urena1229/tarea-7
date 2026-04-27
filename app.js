9b8a82e0e79049b491748bf8301c8d457873d1df
Urena_1229 <miguelur1234@gmail.com>
2026-04-27 13:20:43 -0600

    creacion appjs

---
+++ app.js
@@ -0,0 +1,8 @@
+const moment = require("moment");
+let i = 5;
+let j = 10;
+console.log('iniciaondo....');
+console.log("la suma es " + (5+10));
+
+let fecha = moment().format('LL');
+console.log("la fecha es: ", fecha);
